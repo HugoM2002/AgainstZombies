bee65fd72f29a4f607bb8d9661e825d9615d5a54
HugoM2002 <47594778+HugoM2002@users.noreply.github.com>
2020-02-20 11:14:26 +0100

    Scene

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     public GameObject player;
+    private NavMeshAgent agent;
+    private Renderer renderColor;
 
     private bool aware = false;
 
@@ -15,7 +18,8 @@ public class Enemy : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent.GetComponent<NavMeshAgent>();
+        renderColor = GetComponent<Renderer>();
     }
 
     // Update is called once per frame
@@ -23,17 +27,19 @@ public class Enemy : MonoBehaviour
     {
         if (aware)
         {
-
+            agent.SetDestination(player.transform.position);
+            renderColor.material.color = Color.red;
         }
         else
         {
             SearchForPlayer();
+            renderColor.material.color = Color.black;
         }
     }
 
     public void SearchForPlayer()
     {
-        if(Vector3.Angle(Vector3.forward, transform.InverseTransformPoint(player.transform.position)) < 100f)
+        if(Vector3.Angle(Vector3.forward, transform.InverseTransformPoint(player.transform.position)) < fov / 2f)
         {
             if(Vector3.Distance(player.transform.position, transform.position) < viewDistance)
             {
